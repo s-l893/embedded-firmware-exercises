160090b31ddc8e61f54ee891f6ba7c7e61d1b4cb
s-l893 <sunnylin893@gmail.com>
2026-05-15 23:34:14 -0400

    fixed syntax and structure errors

--- firmware practice G474/Core/Src/main.c
+++ firmware practice G474/Core/Src/main.c
@@ -3,13 +3,23 @@
 
 int main(void)
 {
-    RCC->AHB2ENR |= (1 << 0);
-    GPIOA->MODER &= ~(1 << 11);
-    GPIOA->MODER |= (1 << 10);
+		RCC->APB1ENR1 |= (1 << 17);
+		RCC->AHB2ENR |= (1 << 0);
+		USART2->BRR = 1667;
+		USART2->CR1 |= (1<<2) | (1<<3); // Enable te and re first
+		USART2->CR1 |= (1<<0); // enable usart after
+		GPIOA->MODER |= (1<<5);
+		GPIOA->MODER &= ~(1<<4);
+		GPIOA->MODER |= (1<<7);
+		GPIOA->MODER &= ~(1<<6);
+		GPIOA->AFR[0] |= (7<<8) | (7<<12);
 
-    while(1)
-    {
-        GPIOA->ODR ^= (1 << 5);
-        for(volatile int i = 0; i < 500000; i++);
-    }
+
+
+while  (1){
+	char msg[] = "Hello\n";
+	for (int i = 0; i < 6; i++){
+		while(!(USART2->ISR & (1<<7)));
+		USART2->TDR = msg[i];
+	}}
 }
--- uart exercise.c
+++ uart exercise.c
@@ -3,18 +3,19 @@
 
 int main(void)
 {
+	RCC->APB1ENR1 |= (1 << 17);
+	RCC->AHB2ENR |= (1 << 0);
+	USART2->BRR = 1667;
+	USART2->CR1 |= (1 << 2) | (1 << 3); // Enable te and re first
+	USART2->CR1 |= (1 << 0);			// enable usart after
+	GPIOA->MODER |= (1 << 5);
+	GPIOA->MODER &= ~(1 << 4);
+	GPIOA->MODER |= (1 << 7);
+	GPIOA->MODER &= ~(1 << 6);
+	GPIOA->AFR[0] |= (7 << 8) | (7 << 12);
+
 	while (1)
 	{
-		RCC->APB1ENR1 |= (1 << 17);
-		RCC->AHB2ENR |= (1 << 0);
-		USART2->BRR = 1667;
-		USART2->CR1 |= (1 << 2) | (1 << 3);
-		USART2->CR1 |= (1 << 0)
-						   GPIOA->MODER |= (1 << 5);
-		GPIOA->MODER &= ~(1 << 4);
-		GPIOA->MODER |= (1 << 7);
-		GPIOA->MODER &= ~(1 << 6);
-		GPIOA->AFR[0] |= (7 << 8) | (7 << 12);
 		char msg[] = "Hello\n";
 		for (int i = 0; i < 6; i++)
 		{
